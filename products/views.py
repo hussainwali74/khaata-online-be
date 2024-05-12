017d690d416d11a06dc2e8f75f70524cdaba450b
Hussain Wali <13bscshwali@seecs.edu.pk>
2024-05-12 15:58:01 +0500

    products crud

--- products/views.py
+++ products/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from .models import Product
+from .serializers import ProductSerielizer
 
 # Create your views here.
+class ProductViewSet(viewsets.ViewSet):
+    # handle get request /api/products
+    def list(self, request):
+        products = Product.objects.all()
+        serializer  = ProductSerielizer(products, many=True)
+        return Response(serializer.data)
+    # handle post request /api/products
+    def create(self, request):
+        serializer = ProductSerielizer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    # handle post request /api/products/<str:id>
+    def retrieve(self, request, pk=None):
+        product = Product.objects.get(id=pk)
+        serializer=  ProductSerielizer(product)
+        return Response(serializer.data)
+    # handle put request /api/products/<str:id>
+    def update(self, request, pk=None):
+        product = Product.objects.get(id=pk)
+        serializer = ProductSerielizer(instance=product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(data=serializer.data, status=status.HTTP_202_ACCEPTED)
+    
+    # handle delete  request /api/products/<str:id>
+    def destroy(self, request, pk=None):
+        product = Product.objects.get(id=pk)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
